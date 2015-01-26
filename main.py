65f3e4e401a3678f2813616c9b0bdf1a3e29703a
dilpreetC <dilpreet_chana@hotmail.com>
2015-01-26 00:57:11 -0500

    Finished copy

--- main.py
+++ main.py
@@ -10,25 +10,15 @@ from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import asksaveasfilename
 init()
 
-'''
-TODO:
-* resize stamps
-* fix ellipse
-* blur
-* invert
-* greyshift
-* greyscale near colour palette
-* crop
-'''
-
 screen = display.set_mode((1280, 900))
-display.set_caption("Star Trek Paint")
+display.set_caption("Star Trek Paint")  # Set title of window
 
+# load colour palette and title images
 colour_palette = image.load("colour_palette.jpg")
 star_trek_logo = image.load("title.jpg")
 star_trek_logo = transform.smoothscale(star_trek_logo,(218,100))
 
-# initialize screen
+#---------------------initialize screen---------------------------
 screen.fill((0, 0, 0))
 
 screen.blit(colour_palette, (10, 650))
@@ -42,7 +32,7 @@ root.withdraw()
 
 #--------------create Rects---------------------------
 can_rect = Rect(383, 80, 828, 550)  # canvas Rect
-tool_area_rect = Rect(0, 80, 220, 390)  # Rect of area of selectable tools
+tool_area_rect = Rect(0, 80, 220, 450)  # Rect of area of selectable tools
 music_rect = Rect(175, 595, 45, 45)  # music pause/play button Rect
 colour_area = Rect(10, 650, 200, 200)  # Rect of area of colour palette
 tab_rect = Rect(0, 0, 383, 80)  # Rect of area of tabs
@@ -52,19 +42,16 @@ redo_rect = Rect(120, 595, 45, 45)  # redo button Rect
 clear_rect = Rect(175, 540, 45, 45)  # clear button Rect
 save_rect = Rect(1216, 80, 60, 60)  # save button Rect
 load_rect = Rect(1216, 150, 60, 60)  # load button Rect
-#new_layer = Rect(1151, 645, 60, 60)
-#delete_layer = Rect(1151, 715, 60, 60)
-#layer_area = Rect(383, 645, 758, 240)  # height of each layer rep is 110 and width is 165, max 8 layers
 
 # list of images to be on the background
 back_images = [transform.scale(image.load("cap kirk back.png"), (280, 500)), transform.scale(image.load("spock_back.png"), (209, 270)), transform.scale(image.load("starship back.png"), (366, 207))]
-# 209, 270
 
 # display backround images
 screen.blit(back_images[0], (160, 10))
 screen.blit(back_images[1], (1071, 630))
 screen.blit(back_images[2], (383, 630))
 
+# create subsurfaces
 canvas = screen.subsurface(can_rect)
 tool_area = screen.subsurface(tool_area_rect)
 discrip_area = screen.subsurface(discrip_rect)
@@ -78,10 +65,8 @@ draw.rect(screen, (214, 182, 54), clear_rect)
 draw.rect(screen, (214, 182, 54), music_rect)
 draw.rect(screen, (214, 182, 54), save_rect)
 draw.rect(screen, (214, 182, 54), load_rect)
-#draw.rect(screen, (214, 182, 54), new_layer)
-#draw.rect(screen, (214, 182, 54), delete_layer)
 
-# font stuff
+# create font object
 display_font = font.SysFont("kaiti", 20)
 
 clock = time.Clock()
@@ -95,14 +80,12 @@ old_pos = []
 been = []
 checked = []
 tabs = []
-effect_rects = []
-layer_spots = []
-layers = [screen.subsurface(can_rect)]  # holds all layers added by user
 
 # image lists
-symbols = [image.load("Symbols/command insigna.png"),image.load("Symbols/medical insigna.png"),image.load("Symbols/operations insigna.png"),
-           image.load("Symbols/science insigna.png"),image.load("Symbols/united federation of planets.png"),image.load("Starship/starship 1.png"),
-           image.load("Starship/starship 2.png"),image.load("Starship/starship 3.png")]
+symbols = [image.load("Symbols/command insigna.png"), image.load("Symbols/medical insigna.png"), image.load("Symbols/operations insigna.png"),
+           image.load("Symbols/science insigna.png"), image.load("Symbols/united federation of planets.png"), image.load("Starship/starship 1.png"),
+           image.load("Starship/starship 2.png"), image.load("Starship/starship 3.png")]
+
 stamp_display = [image.load("Symbols/command insigna.png"),image.load("Symbols/medical insigna.png"),image.load("Symbols/operations insigna.png"),
                 image.load("Symbols/science insigna.png"),image.load("Symbols/united federation of planets.png"),image.load("Starship/starship 1.png"),
                 image.load("Starship/starship 2.png"),image.load("Starship/starship 3.png")]
@@ -110,7 +93,8 @@ stamp_display = [image.load("Symbols/command insigna.png"),image.load("Symbols/m
 tool_images = [image.load("tool icon/pencil icon.png"), image.load("tool icon/eraser icon.png"), image.load("tool icon/brush icon.png"), image.load("tool icon/spray icon.png"),
                image.load("tool icon/fill icon.png"), image.load("tool icon/polygon icon.png"), image.load("tool icon/eye drop icon.png"), image.load("tool icon/clear icon.png"),
                image.load("tool icon/cut icon.png"), image.load("tool icon/load icon.png"), image.load("tool icon/save icon.png"), image.load("tool icon/text icon.png"), 
-               image.load("tool icon/tool tab icon.png"), image.load("tool icon/stamp tab icon.png"), image.load("tool icon/undo icon.png"), image.load("tool icon/redo icon.png")]
+               image.load("tool icon/tool tab icon.png"), image.load("tool icon/stamp tab icon.png"), image.load("tool icon/undo icon.png"), image.load("tool icon/redo icon.png"),
+               image.load("tool icon/play icon.png")]
 
 # tool descriptions
 description = ["Pencil: Take notes on any new planets", "Eraser: Blast away your mistakes", "Brush: Paint with a brush", "Spray: Spray can tool", "Fill: Click to fill closed areas", 
@@ -122,7 +106,8 @@ for i in range(len(symbols)-1):
     transform.scale(symbols[i],(70,100))
 
 # Create Rects for images
-symbol_rect = [Rect(10,10,70,100),Rect(90,10,70,100),Rect(170,10,70,100),Rect(10,120,70,100),Rect(90,120,100,100)]
+symbol_rect = [Rect(10, 10, 70, 100), Rect(90, 10, 70, 100), Rect(10, 120, 70, 100), Rect(90, 120, 70, 100), 
+			   Rect(10, 230, 70, 51), Rect(90, 230, 112, 68), Rect(10, 340, 100, 64), Rect(90, 340, 112, 68)]
 
 # initialize variables
 tool = tools[0]
@@ -139,27 +124,19 @@ text = ""
 typing = False
 effect = "blur"
 copy = canvas.copy()
-#current_layer = layers[0]
-#star_trek_font = font.SysFont("Comic Sans MS",20)
 
 # create tool Rects
 for i in range(5):
     for j in range(3):
         tool_rect.append(Rect(10 + 70 * j, 70 * i, 60, 60))
 
-# create effect Rects
-for i in range(3):
-    effect_rects.append(Rect(10, 90 * i, 150, 80)),
-
 # create tab Rects
-for i in range(4):
+for i in range(2):
     tabs.append(Rect(10+70*i, 10, 60, 60))
 
-for x in range(0, 928, 170):
-    for y in range(0, 120, 115):
-        layer_spots.append((x,y))
-
+# Draw undo/redo/play/clear images
 screen.blit(tool_images[7], (180, 545))
+screen.blit(tool_images[16], (180, 600))
 screen.blit(tool_images[14], (125, 545))
 screen.blit(tool_images[15], (125, 600))
 
@@ -171,9 +148,9 @@ while running:
         if e.type == QUIT:
             running = False
         if e.type == MOUSEBUTTONDOWN:
-            init_x,init_y = mouse.get_pos()
+            init_x,init_y = mouse.get_pos()  # used in rectengle type tools to find topleft corner
 
-            if can_rect.collidepoint((mx,my)):
+            if can_rect.collidepoint((mx,my)):  # set redo blank if the user clicks on the canvas
                 redo = []
 
             if e.button == 4:
@@ -184,29 +161,32 @@ while running:
 
             #-------------Tools with MOUSEDOWN---------------------------
             if can_rect.collidepoint((mx,my)) and tool == "polygon" and e.button == 1:
-                been = polygonPoints(canvas, can_x, can_y, brush_size, brush_colour, been)
+                been = polygonPoints(canvas, can_x, can_y, brush_size, brush_colour, been)  # add the points of a polygon to a list
 
             if e.button == 4 and selected_tab == "stamps":
                 try:
+                	# Increase image size when scroll up
                     display_image = transform.scale(symbols[stamp_num], (int(symbols[stamp_num].get_width() + brush_size * 5), int(symbols[stamp_num].get_height() + brush_size * 5)))
                     screen.blit(copy, (can_off_x, can_off_y))
                 except Exception:
                     pass
             elif e.button == 5 and selected_tab == "stamps":
                 try:
+                	# Decrease image size when scroll down
                     display_image = transform.scale(symbols[stamp_num], (int(symbols[stamp_num].get_width() - brush_size * 5), int(symbols[stamp_num].get_height() - brush_size * 5)))
                     screen.blit(copy, (can_off_x, can_off_y))
                 except Exception:
                     pass
 
             elif e.button == 1 and selected_tab == "stamps":
+            	# Draw image to canvas when user clicks
                 canvas.blit(display_image,(can_x-display_image.get_width()//2,can_y-display_image.get_height()//2))
 
             #-----------------------undo, redo, clear-------------------------------------
-            if clear_rect.collidepoint((mx,my)):
+            if clear_rect.collidepoint((mx,my)):  # clear canvas
                 canvas.fill((255,255,255))
 
-            if undo_rect.collidepoint((mx,my)) and len(undo_rect) > 1:
+            if undo_rect.collidepoint((mx,my)) and len(undo_rect) > 1:  # undo
                 try:
                     screen.blit(undo[-2],(can_off_x,can_off_y))
                     redo.append(undo[-1])
@@ -214,7 +194,7 @@ while running:
                 except Exception:
                     pass
 
-            if redo_rect.collidepoint((mx,my)) and len(redo_rect) > 0:
+            if redo_rect.collidepoint((mx,my)) and len(redo_rect) > 0:  # redo
                 try:
                     screen.blit(redo[-1],(can_off_x,can_off_y))
                     undo.append(redo[-1])
@@ -223,11 +203,11 @@ while running:
                     pass
 
             #--------------toggle music----------------------------------
-            if music and music_rect.collidepoint((mx,my)):
+            if music and music_rect.collidepoint((mx,my)):  # turn music off
                 music = False
                 mixer.music.fadeout(100)
                 draw.rect(screen, (255, 0, 0), music_rect)
-            elif music == False and music_rect.collidepoint((mx,my)):
+            elif music == False and music_rect.collidepoint((mx,my)):  # turn music on
                 music = True
                 mixer.music.play(-1,0.0)
                 draw.rect(screen, (214, 182, 54), music_rect)
@@ -235,7 +215,7 @@ while running:
             #----------------save annd load-----------------------------
             if save_rect.collidepoint((mx,my)) and e.button == 1:
                 fileName = asksaveasfilename(parent=root,title="Save the image as...")
-                if fileName[len(fileName)-1:len(fileName)-5:-1] != ".png" or fileName[len(fileName)-1:len(fileName)-5:-1] != ".jpg":  # Check if file has propor extension
+                if fileName[len(fileName)-1:len(fileName)-5:-1] != ".png" or fileName[len(fileName)-1:len(fileName)-5:-1] != ".jpg":  # Check if file has propor extension if not add .png
                     fileName += ".png"
                 image.save(canvas, fileName)
 
@@ -254,7 +234,9 @@ while running:
                 text_pos = (can_x, can_y)
                 typing = True
             if typing:
-                if e.type == KEYDOWN:
+            	if e.type == MOUSEBUTTONDOWN:
+            		screen.blit(copy, (can_off_x, can_off_y))
+            	if e.type == KEYDOWN:
                     if e.key == K_BACKSPACE:
                         text = text[:-1]
                     elif e.key == K_RETURN:
@@ -272,7 +254,7 @@ while running:
             
     mx,my = mouse.get_pos()
 
-    can_x = mx-can_off_x
+    can_x = mx-can_off_x  # x, y of mouse on canvas
     can_y = my-can_off_y
 
     tool_area_x = mx
@@ -280,7 +262,7 @@ while running:
 
     mb = mouse.get_pressed()
 
-    texttool_font = font.SysFont("kaiti", int(brush_size))
+    texttool_font = font.SysFont("kaiti", int(brush_size))  # font for the text tool
 
     # display brush size and tool information
     discrip_area.fill((0, 0, 0))
@@ -289,7 +271,7 @@ while running:
     discrip_area.blit(display_text, (0, 40))
     discrip_area.blit(display_size, (0, 20))
 
-    #--------------do stuff--------------------------------------
+    #--------------tools do stuff--------------------------------------
     if can_rect.collidepoint((mx,my)):
         if mb[0] == 1 and tool == "pencil":
             pencil(screen, canvas, old_pos, can_x, can_y, brush_colour)
@@ -323,7 +305,7 @@ while running:
             circle(screen, canvas, copy, can_off_x, can_off_y, init_x, init_y, mx, my, brush_size, brush_colour, filled)
 
         if mb[0] == 1 and tool == "ellipse":
-            ellipse(screen, canvas, copy, can_off_x, can_off_y, init_x, init_y, mx, my, brush_size, brush_colour, filled)
+            ellipse(screen, canvas, copy, can_off_x, can_off_y, init_x, init_y, mx, my, can_x, can_y, brush_size, brush_colour, filled)
 
         if mb[0] == 1 and tool == "eye drop":
             brush_colour = eyeDrop(canvas, can_x, can_y)
@@ -333,14 +315,6 @@ while running:
             canvas.blit(canvas_text,(0,0))
             canvas.blit(typing_text_pic, text_pos)
 
-        '''
-        if tool == "select":
-            if mb[0] == 1 and copied == False:
-                selected_copy = selectCopy(screen, canvas, copy, can_off_x, can_off_y, init_x, init_y, mx, my, mb, brush_size, brush_colour)
-            if 1 not in mb and copied == False:
-                copied = True
-        '''
-
         if 1 not in mb:
             old_pos = []
 
@@ -348,14 +322,20 @@ while running:
     if selected_tab == "stamps":
         if can_rect.collidepoint((mx,my)):
             screen.blit(copy, (can_off_x,can_off_y))
-            canvas.blit(display_image,(can_x-width//2,can_y-height//2))
+            canvas.blit(display_image,(can_x-width//2,can_y-height//2))  # center image on mouse
         else:
             screen.blit(copy, (can_off_x,can_off_y))
 
         width = symbols[stamp_num].get_width()
         height = symbols[stamp_num].get_height()
 
-        
+    #---------------------------colour change-------------------------------------------
+    draw.rect(screen, brush_colour, (10, 540, 100, 100))
+    if colour_area.collidepoint((mx, my)) and mb[0] == 1:
+        brush_colour = screen.get_at((mx, my))
+        screen.blit(colour_palette, (10, 650))
+        draw.circle(screen, (0, 0, 0), (mx, my), 3, 1)
+
 
     if can_rect.collidepoint((mx,my)) == False:  # things to do off of the canvas
 
@@ -369,11 +349,6 @@ while running:
             draw.rect(screen, (0, 255, 0), tabs[1])
         else:
             draw.rect(screen, (214, 182, 54), tabs[1])
-
-        if selected_tab == "effect":
-            draw.rect(screen, (0, 255, 0), tabs[2])
-        else:
-            draw.rect(screen, (214, 183, 54), tabs[2])
 
         #--------------display basic tools---------------------------
         if selected_tab == "basic tools":
@@ -470,23 +445,6 @@ while running:
             draw.rect(tool_area, (0, 0, 0), (86, 285, 48, 48))
             draw.rect(tool_area, (0, 0, 0), (156, 285, 48, 48), 3)
 
-        #------------------display effects--------------------------------------------------
-        if selected_tab == "effects":
-            if effect == "blur":
-                draw.rect(tool_area, (0, 255, 0), effect_rects[0])
-            else:
-                draw.rect(tool_area, (214, 182, 54), effect_rects[0])
-
-            if effect == "invert":
-                draw.rect(tool_area, (0, 255, 0), effect_rects[1])
-            else:
-                draw.rect(tool_area, (214, 182, 54), effect_rects[1])
-
-            if effect == "gray":
-                draw.rect(tool_area, (0, 255, 0), effect_rects[2])
-            else:
-                draw.rect(tool_area, (214, 182, 54), effect_rects[2])
-
 
         #-------------------tool change-----------------------------------------------------
         if mb[0] == 1 and selected_tab == "basic tools":
@@ -521,30 +479,22 @@ while running:
             if tool_rect[14].collidepoint((tool_area_x, tool_area_y)):
                 filled = False
 
-        print(tool,mx,my)
-
         #------------------stamp change-----------------------------------------------------
         if selected_tab == "stamps":
             tool_area.blit(stamp_display[0],(10,10))
             tool_area.blit(stamp_display[1],(90,10))
-            tool_area.blit(stamp_display[2],(170,10))
-            tool_area.blit(stamp_display[3],(10,120))
-            tool_area.blit(stamp_display[4],(90,120))
+            tool_area.blit(stamp_display[2],(10,120))
+            tool_area.blit(stamp_display[3],(90,120))
+            tool_area.blit(stamp_display[4],(10,230))
+            tool_area.blit(transform.scale(stamp_display[5], (112, 68)), (90, 230))
+            tool_area.blit(transform.scale(stamp_display[6], (100, 64)), (10, 340))
+            tool_area.blit(transform.scale(stamp_display[7], (112, 68)), (90, 340))
 
             for i in range(len(symbol_rect)):
                 if symbol_rect[i].collidepoint((tool_area_x,tool_area_y)) and mb[0] == 1:
                     stamp_num = i
 
-            display_image = transform.smoothscale(symbols[stamp_num], (symbols[stamp_num].get_width(), symbols[stamp_num].get_height()))
-
-        #------------------effect change----------------------------------------------------
-        if mb[0] == 1 and selected_tab == "effect_rects":
-            if effect_rects[0].collidepoint((mx,my)):
-                effect = "blur"
-            if effect_rects[1].collidepoint((mx,my)):
-                effect = "invert"
-            if effect_rects[2].collidepoint((mx,my)):
-                effect = "gray"
+            display_image = transform.scale(symbols[stamp_num], (symbols[stamp_num].get_width(), symbols[stamp_num].get_height()))
 
         # display tab icons
         screen.blit(tool_images[12], (12, 12))
@@ -558,23 +508,9 @@ while running:
         if tabs[1].collidepoint((mx,my)):
             tool_area.fill((0,0,0))
             selected_tab = "stamps"
-        if tabs[2].collidepoint((mx,my)):
-            tool_area.fill((0,0,0))
-            selected_tab = "effect"
 
-    #---------------------------colour change-------------------------------------------
-    draw.rect(screen, (255,255,255), (10, 540, 100, 100))
-    draw.circle(screen,brush_colour,(60,590),int(brush_size/2))
-    if colour_area.collidepoint((mx, my)) and mb[0] == 1:
-        brush_colour = screen.get_at((mx, my))
-        screen.blit(colour_palette, (10, 650))
-        draw.circle(screen, (0, 0, 0), (mx, my), 3, 1)
-
-
-    display.set_caption("Star Trek Paint fps: " + str(round(clock.get_fps(),2)))
     clock.tick(60)
     display.flip()
 
-#font.quit()
 mixer.quit()
 quit()
--- tools.py
+++ tools.py
@@ -25,11 +25,11 @@ def brush(screen, canvas, old_pos, x, y, size, colour):
         draw.circle(canvas,colour,(x,y),int(size/2))
 
 def spray(canvas, x, y, size, colour):
-    for i in range(5):
-        spray_x = x - randint(-(int(size/2)), int(size/2))
+    for i in range(15):
+        spray_x = x - randint(-(int(size/2)), int(size/2))  # get posible x, y poits to draw
         spray_y = y - randint(-(int(size/2)), int(size/2))
         radius = int(size/2)
-        if sqrt((spray_x-x)**2 + (spray_y-y)**2) <= radius:
+        if sqrt((spray_x-x)**2 + (spray_y-y)**2) <= radius:  # check if the points are within the radius of the circle
             canvas.set_at((spray_x, spray_y), colour)
 
 def flood_fill(screen, canvas, can_rect, mpos, x, y, size, colour):
@@ -40,8 +40,8 @@ def flood_fill(screen, canvas, can_rect, mpos, x, y, size, colour):
         while len(loc) > 0:
             cx, cy = loc.pop()
             if screen.get_at((cx, cy)) == sel_col and can_rect.collidepoint((cx, cy)):
-                screen.set_at((cx, cy), colour)
-                loc.append((cx+1, cy))
+                screen.set_at((cx, cy), colour)  # fill current point
+                loc.append((cx+1, cy))  # check the points all around the current point
                 loc.append((cx-1, cy))
                 loc.append((cx, cy+1))
                 loc.append((cx, cy-1))
@@ -70,6 +70,7 @@ def square(screen, canvas, copy, can_off_x, can_off_y, init_x, init_y, mx, my, s
         if init_x-mx>=init_y-my:
             draw.rect(canvas,colour,(init_x-can_off_x,init_y-can_off_y,-(init_x-mx),-(init_x-mx)),int(size))
 
+            # draw corner rectangles to complete shape
             draw.rect(canvas,colour,(init_x-can_off_x-int(size/2),init_y-can_off_y-int(size/2),int(size),int(size)))
             draw.rect(canvas,colour,(init_x-can_off_x-int(size/2)+(mx-init_x),init_y-can_off_y-int(size/2)+(mx-init_x),int(size),int(size)))
             draw.rect(canvas,colour,(init_x-can_off_x-int(size/2)+(mx-init_x),init_y-can_off_y-int(size/2),int(size),int(size)))
@@ -115,25 +116,17 @@ def circle(screen, canvas, copy, can_off_x, can_off_y, init_x, init_y, mx, my, s
             size = 2
         draw.circle(canvas,colour,(init_x-can_off_x,init_y-can_off_y),int(radius+size/2))
 
-def ellipse(screen, canvas, copy, can_off_x, can_off_y, init_x, init_y, mx, my, size, colour, filled):
+def ellipse(screen, canvas, copy, can_off_x, can_off_y, init_x, init_y, mx, my, can_x, can_y, size, colour, filled):
     screen.blit(copy,(can_off_x,can_off_y))
-    if filled == False:
-        if init_x <= mx or init_y <= my:
+    try:
+        if init_x <= mx and init_y <= my:
             draw.ellipse(canvas,colour,(init_x-can_off_x,init_y-can_off_y,(mx-init_x),(my-init_y)))
-        elif init_x > mx or init_y > my:
+        elif init_x > mx and init_y > my:
             draw.ellipse(canvas,colour,(can_x,can_y,(mx-init_x),(my-init_y)))
-    if filled:
-        if init_x <= mx or init_y <= my:
-            draw.ellipse(canvas,colour,(init_x-can_off_x,init_y-can_off_y,(mx-init_x),(my-init_y)))
-        elif init_x > mx or init_y > my:
-            draw.ellipse(canvas,colour,(can_x,can_y,(mx-init_x),(my-init_y)))
+    except Exception:
+        pass
 
 def eyeDrop(canvas, x, y):
     colour = canvas.get_at((x,y))
 
     return colour
-
-def selectCopy(screen, canvas, copy, can_off_x, can_off_y, init_x, init_y, mx, my, mb, size, colour):
-    screen.blit(copy, (can_off_x, can_off_y))
-    select_rect = (init_x, init_y, mx-init_x, my-init_y)
-    draw.rect(screen, (0, 0, 0), select_rect)
